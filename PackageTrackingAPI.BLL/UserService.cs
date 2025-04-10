57a567921244dbdca45fa2767ffb5aac4357e02c
Duan <daniel.w2301@gmail.com>
2025-04-10 18:43:34 -0500

    add error handling

--- PackageTrackingAPI.BLL/UserService.cs
+++ PackageTrackingAPI.BLL/UserService.cs
@@ -1,62 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AutoMapper;
-using Microsoft.EntityFrameworkCore;
+﻿using AutoMapper;
 using PackageTrackingAPI.DAL;
 using PackageTrackingAPI.DTOs;
 using PackageTrackingAPI.Models;
 
-namespace PackageTrackingAPI.BLL
+public class UserService
 {
-    public class UserService
+    private readonly UserRepository _userRepository;
+    private readonly IMapper _mapper;
+
+    public UserService(UserRepository userRepository, IMapper mapper)
     {
-        private readonly UserRepository _userRepository;
-        private readonly IMapper _mapper;
+        _userRepository = userRepository;
+        _mapper = mapper;
+    }
 
-        public UserService(UserRepository userRepository, IMapper mapper)
+    public async Task<List<UserDto>> GetAllUsersAsync()
+    {
+        var users = await _userRepository.GetAllUsersAsync();
+        return _mapper.Map<List<UserDto>>(users);
+    }
+
+    public async Task<UserDto> GetUserByIdAsync(int id)
+    {
+        var user = await _userRepository.GetUserByIdAsync(id);
+        if (user == null)
         {
-            _userRepository = userRepository;
-            _mapper = mapper;
+            throw new KeyNotFoundException($"User with ID {id} not found.");
+        }
+        return _mapper.Map<UserDto>(user);
+    }
+
+    public async Task<UserDto> CreateUserAsync(UserDto userDto)
+    {
+        if (await _userRepository.EmailExistsAsync(userDto.Email))
+        {
+            throw new ArgumentException("A user with this email already exists.");
         }
 
-        public async Task<UserDto> CreateUserAsync(UserDto userDto)
+        var user = _mapper.Map<User>(userDto);
+        await _userRepository.CreateUserAsync(user);
+        return _mapper.Map<UserDto>(user);
+    }
+
+    public async Task<UserDto> UpdateUserAsync(int id, UserDto userDto)
+    {
+        var user = await _userRepository.GetUserByIdAsync(id);
+        if (user == null)
         {
-            var user = _mapper.Map<User>(userDto);
-            var createdUser = await _userRepository.CreateUserAsync(user);
-            return _mapper.Map<UserDto>(createdUser);
+            throw new KeyNotFoundException($"User with ID {id} not found.");
         }
 
-        public async Task<List<UserDto>> GetAllUsersAsync()
+        _mapper.Map(userDto, user);
+        await _userRepository.UpdateUserAsync(user);
+
+        return _mapper.Map<UserDto>(user);
+    }
+
+    public async Task DeleteUserAsync(int id)
+    {
+        var user = await _userRepository.GetUserByIdAsync(id);
+        if (user == null)
         {
-            var users = await _userRepository.GetAllUsersAsync();
-            return _mapper.Map<List<UserDto>>(users);
+            throw new KeyNotFoundException($"User with ID {id} not found.");
         }
 
-        public async Task<UserDto> GetUserByIdAsync(int id)
-        {
-            var user = await _userRepository.GetUserByIdAsync(id);
-            return user == null ? null : _mapper.Map<UserDto>(user);
-        }
-
-        public async Task<UserDto> UpdateUserAsync(int id, UserDto userDto)
-        {
-            var existingUser = await _userRepository.GetUserByIdAsync(id);
-            if (existingUser == null)
-            {
-                throw new KeyNotFoundException($"User with ID {id} not found.");
-            }
-
-            _mapper.Map(userDto, existingUser);
-            var updatedUser = await _userRepository.UpdateUserAsync(existingUser);
-            return _mapper.Map<UserDto>(updatedUser);
-        }
-
-        public async Task<bool> DeleteUserAsync(int id)
-        {
-            return await _userRepository.DeleteUserAsync(id);
-        }
+        await _userRepository.DeleteUserAsync(id);
     }
 }
